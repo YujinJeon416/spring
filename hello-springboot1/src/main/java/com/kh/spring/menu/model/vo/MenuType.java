596915dd499335b59d16600edd2c4b98809e2ead
YujinJeon416 <75886211+YujinJeon416@users.noreply.github.com>
2021-05-28 21:53:53 +0900

    21/05/28

---
+++ hello-springboot1/src/main/java/com/kh/spring/menu/model/vo/MenuType.java
@@ -0,0 +1,38 @@
+package com.kh.spring.menu.model.vo;
+
+/**
+ * 
+ * java.lang.Enum을 상속
+ * 
+ *
+ */
+public enum MenuType {
+	
+	KR("kr"), CH("ch"), JP("jp");
+	
+	private String value;
+	
+	/**
+	 * enum 생성자의 접근제한자는 private
+	 * 외부에서 접근/생성 불가
+	 * 
+	 * 
+	 */
+	MenuType(String value){
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return this.value;
+	}
+	public static MenuType menuTypeValueOf(String value) {
+		switch(value) {
+		case "kr" : return KR;
+		case "ch" : return CH;
+		case "jp" : return JP;
+		default:
+			throw new AssertionError("Unknown MenuType : " + value);
+		}
+	}
+	}
+
